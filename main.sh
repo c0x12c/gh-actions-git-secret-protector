4b6de8129611fd793185709c0b0797d4597a526d
Duc Duong <duc.duong@c0x12c.com>
2024-09-15 13:22:16 +0700

    Remove debugging code

--- main.sh
+++ main.sh
@@ -9,14 +9,6 @@ if [ -z "$FILTER_NAME" ]; then
     exit 1
 fi
 
-export PATH="/root/.local/bin:$PATH"
-
-ls -la /root/.local/bin
-
-# Debugging the PATH to ensure git-secret-protect is in it
-echo "PATH: $PATH"
-which git-secret-protect || { echo "git-secret-protect not found"; exit 1; }
-
 echo "Decrypting files using filter: $FILTER_NAME"
 
 git-secret-protector decrypt-files "$FILTER_NAME"
