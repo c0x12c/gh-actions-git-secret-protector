c82748dc1266a3c3ce5515aee87e35507c59a9aa
Duc Duong <duc.duong@c0x12c.com>
2024-09-15 13:24:31 +0700

    Debugging

--- main.sh
+++ main.sh
@@ -9,6 +9,10 @@ if [ -z "$FILTER_NAME" ]; then
     exit 1
 fi
 
+# Debugging the PATH to ensure git-secret-protect is in it
+ls -la /root/.local/bin
+echo "PATH: $PATH"
+
 echo "Decrypting files using filter: $FILTER_NAME"
 
 git-secret-protector decrypt-files "$FILTER_NAME"
